9da186aacd0e721e05d81ae1eac9a8182ba90eaa
kevin <kevin.rojokevin17@gmail.com>
2024-06-12 19:47:19 -0300

    Terminado

--- js/main.js
+++ js/main.js
@@ -7,6 +7,9 @@ let url = "https://api.api-ninjas.com/v1/quotes?category=" + category;
 
 let titulo = document.querySelector(".autor");
 let texto = document.querySelector(".text");
+let cambiar = document.querySelector(".cambiar");
+let copiar = document.querySelector(".copiar");
+let boton2 = document.querySelector(".btn2");
 
 /*creamos una funcion para obtener Los datos de la API*/
 
@@ -25,13 +28,23 @@ function obtenerDatos() {
 }
 
 function mostrarDatos(data) {
-  if (data.length > 0) {
-    let elementos = data[0];
-    titulo.innerHTML = `<h3 class="title">${
-      elementos.author || "unknown"
-    }</h3>`;
-    texto.innerHTML = `<p class="cita">"${elementos.quote}"</p>`;
+  let elementos = data[0];
+  console.log(elementos);
+  titulo.innerHTML = `<h3 class="title">${elementos.author}</h3>`;
+  texto.innerHTML = `<p class="cita">"${elementos.quote}"</p>`;
+  boton2.innerText = `${elementos.category}`;
+
+  copiar.onclick = () => copiarCita(elementos);
+}
+
+async function copiarCita(cita) {
+  try {
+    await navigator.clipboard.writeText(`"${cita.quote}" - ${cita.author}`);
+    alert("Texto copiado: " + `"${cita.quote}" - ${cita.author}`);
+  } catch (err) {
+    console.error("Error al copiar el texto: ", err);
   }
 }
+cambiar.addEventListener("click", obtenerDatos);
 
 obtenerDatos();
